2a40e1b473d06806e3c93ecc8b0b9e9402009bae
RekLite <raphael.montulet@hotmail.fr>
2022-08-13 20:28:50 +0200

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './styles/App.css'
 import Calendar from './components/Calendar';
 import dayjs from "dayjs";
 
 function App() {
 
-  const currentDate = dayjs();
+  // const currentDate = dayjs();
   const d = new Date(2022, 1, 15);
   let givenDate = dayjs(d);
 
