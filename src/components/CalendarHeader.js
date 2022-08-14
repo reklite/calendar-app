0e26c509df733762655db323e4911441d36cf0f1
RekLite <raphael.montulet@hotmail.fr>
2022-08-14 03:48:28 +0200

    added logic for display of users with corresponding color

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,28 +1,64 @@
-import React from 'react'
+import React from "react";
+// import
 
-const CalendarHeader = (props) => (
+const CalendarHeader = (props) => {
+
+  // added photos locally because of 403 forbidden error on links
+  let pic1 = require("../data/photos/arnaud.jpeg");
+  let pic2 = require("../data/photos/alban.jpeg");
+  const usersPhotos = [pic1, pic2];
+
+  if (props.users !== null) console.log(props.users[0].avatar);
+
+  const getStyle = (color) => {
+    const style = {
+      content: "",
+      width: "35px",
+      height: "30px",
+      marginLeft: "-15px",
+      borderRadius: "5px 0 0 5px",
+      backgroundColor: color,
+      transform: "translateX(15px)"
+  };
+
+  return style;
+}
+
+  return (
     <section className="calendar-month-header">
-          <span
-            onClick={() => props.changeDate(props.date, "left")}
-            className="calendar-month-header-selectors"
-            id="previous-month-selector"
-          >
-            {"<"}
-          </span>
-          <div
-            id="selected-month"
-            className="calendar-month-header-selected-month"
-          >
-            {props.currMonthName} {props.currYear}
-          </div>
-          <span
-            onClick={() => props.changeDate(props.date, "right")}
-            className="calendar-month-header-selectors"
-            id="next-month-selector"
-          >
-            {">"}
-          </span>
-        </section>
-  )
+      <div className="users">
+        {props.users &&
+          props.users.map((user, i) => (
+            <>
+            <div style={getStyle(user.color)}></div>
+              <div className="user-block">
+                <img className="user-photo" src={usersPhotos[i]} alt={user.name} />
+                <span key={user.name}>
+                  {user.name}
+                </span>
+              </div>
+            </>
+          ))}
+      </div>
+      <span
+        onClick={() => props.changeDate(props.date, "left")}
+        className="calendar-month-header-selectors"
+        id="previous-month-selector"
+      >
+        {"<"}
+      </span>
+      <div id="selected-month" className="calendar-month-header-selected-month">
+        {props.currMonthName} {props.currYear}
+      </div>
+      <span
+        onClick={() => props.changeDate(props.date, "right")}
+        className="calendar-month-header-selectors"
+        id="next-month-selector"
+      >
+        {">"}
+      </span>
+    </section>
+  );
+};
 
-export default CalendarHeader
+export default CalendarHeader;
